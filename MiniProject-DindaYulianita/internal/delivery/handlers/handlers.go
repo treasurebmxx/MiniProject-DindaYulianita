8c148b646336fb85a341a8ea1798779ca7102f00
Dinda Yulianita <88498468+treasurebmxx@users.noreply.github.com>
2025-10-27 15:53:34 +0700

    handlers.go

--- MiniProject-DindaYulianita/internal/delivery/handlers/handlers.go
+++ MiniProject-DindaYulianita/internal/delivery/handlers/handlers.go
@@ -4,12 +4,12 @@ import (
 	"MiniProject-DindaYulianita/internal/usecase"
 )
 
-// Handler utama yang menyimpan semua handler spesifik
+
 type Handler struct {
 	AuthHandler *AuthHandler
 }
 
-// NewHandler membuat instance handler baru
+
 func NewHandler(uc *usecase.Usecase) *Handler {
 	return &Handler{
 		AuthHandler: NewAuthHandler(uc),
